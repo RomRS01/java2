d95f6bc3277593848e183d8d25476541950ebada
Roman Rogachev <romanaddress@mail.ru>
2022-09-04 01:14:56 +0300

    Изменение переменных

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int stoimost = 10000;
-        int miljaRub = 20;
-        int mili;
-        if (miljaRub != 0) {
-            mili = stoimost / miljaRub;
-            System.out.println(mili + " начисленных милей");
+        int cost = 10000;
+        int mileRub = 20;
+        int mile;
+        if (mileRub != 0) {
+            mile = cost / mileRub;
+            System.out.println(mile + " начисленных милей");
         } else {
             System.out.println("миля не может соответствовать 0 рублям");
         }
